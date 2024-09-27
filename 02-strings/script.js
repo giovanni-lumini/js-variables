6fc8ebd02135d21f87d2d20bbb8e7f90c95ec168
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-09-27 14:34:18 +0200

    strings-pt-1-2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,18 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+name = "giovanni";
+console.log(name);
+
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+number = 6;
+text = "iovann";
+bool = 1;
+console.log(number+text+bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
