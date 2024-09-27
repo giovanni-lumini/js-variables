741943590c895c615a9fbe0f84df37dfd4dde7d4
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-09-27 14:26:53 +0200

    numbers-pt-2-3-4-5-6

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -12,21 +12,31 @@ let numOnePlusNumTwo = numOne+numTwo
 console.log(numOnePlusNumTwo);
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
-const numThreeTimesNumTwo = NaN;
-console.log(numThreeTimesNumTwo); // Output: 512
+/* const numThreeTimesNumTwo = NaN;
+console.log(numThreeTimesNumTwo); */ // Output: 512
+let numThreeTimesNumTwo = numThree*numTwo;
+console.log(numThreeTimesNumTwo);
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
-const numThreeDividedByNumOne = NaN;
-console.log(numThreeDividedByNumOne); // Output: 4
+/* const numThreeDividedByNumOne = NaN;
+console.log(numThreeDividedByNumOne); */ // Output: 4
+numThreeDividedByNumOne = numThree/numOne;
+console.log(numThreeDividedByNumOne);
 
 // 4. Imposta questa variabile come differenza tra numThree e numOne
-const numThreeMinusNumOne = NaN;
-console.log(numThreeMinusNumOne); // Output: 4
+/* const numThreeMinusNumOne = NaN;
+console.log(numThreeMinusNumOne); */ // Output: 4 /* è sbagliato 4, fa 24 */
+numThreeMinusNumOne = numThree-numOne;
+console.log(numThreeMinusNumOne);
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = NaN;
-console.log(sum); // Output: 56
+/* const sum = NaN;
+console.log(sum); */ // Output: 56
+sum = numOne+numTwo+numThree;
+console.log(sum);
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = NaN;
-console.log(numBytes); // Output: 7
+/* const numBytes = NaN;
+console.log(numBytes); */ // Output: 7
+numBytes = (numOne+numTwo+numThree)/numOne;
+console.log(numBytes);
